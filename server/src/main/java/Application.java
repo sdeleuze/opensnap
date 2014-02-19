4bef7bc783d7205841cc242b562cdf4d52ae9cc2
Sebastien Deleuze <sdeleuze@gopivotal.com>
2014-02-19 15:10:10 +0100

    Upgrade to Spring Boot 1.0.0.RC3 and Spring 4.0.2.RELEASE

--- server/src/main/java/Application.java
+++ server/src/main/java/Application.java
@@ -1,9 +1,8 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.SpringBootServletInitializer;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
