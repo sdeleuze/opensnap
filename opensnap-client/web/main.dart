c900be14801c45394451d4c18e72f6080823466b
Sebastien Deleuze <sdeleuze@gopivotal.com>
2014-05-07 17:22:43 +0200

    Spring and AngularDart update

--- opensnap-client/web/main.dart
+++ opensnap-client/web/main.dart
@@ -9,17 +9,17 @@ import 'dart:html';
 class OpenSnapModule extends Module {
   OpenSnapModule() {
     install(new AngularUIModule());
-    type(StompClientService);
-    type(UserService);
-    type(SnapService);
-    type(NavbarComponent);
-    type(SigninComponent);
-    type(PhotoComponent);
-    type(SnapsReceivedComponent);
-    type(SnapsSentComponent);
-    type(NotifyComponent);
-    type(AdminComponent);
-    value(RouteInitializerFn, opensnapRouteInitializer);
+    bind(StompClientService);
+    bind(UserService);
+    bind(SnapService);
+    bind(NavbarComponent);
+    bind(SigninComponent);
+    bind(PhotoComponent);
+    bind(SnapsReceivedComponent);
+    bind(SnapsSentComponent);
+    bind(NotifyComponent);
+    bind(AdminComponent);
+    bind(RouteInitializerFn, toValue: opensnapRouteInitializer);
   }
 }
 
